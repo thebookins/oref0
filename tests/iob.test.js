da6fc3896842f518ba30d62b7a4f278dbd5be883
Paul Dickens <dickens.paul@gmail.com>
2016-09-03 17:54:55 +1000

    added more iob tests

--- tests/iob.test.js
+++ tests/iob.test.js
@@ -27,30 +27,37 @@ describe('IOB', function ( ) {
           type: 'Bolus'
           , amount: 1
           , start_at: timestamp
+          , end_at: timestamp
+          , unit: "U"
         }]
         , profile: profile
-        , prepared: true 
       };
+      
+    runTest(require('../lib/iob'), inputsDefault);
+    runTest(require('../lib/iob-prepared'), inputsPrepared);
 
-    [inputsDefault, inputsPrepared].forEach(function(inputs) {
-      var rightAfterBolus = require('../lib/iob')(inputs)[0];
+    function runTest(generate, inputs) {
+      var rightAfterBolus = generate(inputs)[0];
       rightAfterBolus.iob.should.equal(1);
       rightAfterBolus.bolussnooze.should.equal(1);
+      rightAfterBolus.activity.should.equal(0);
 
       var hourLaterInputs = inputs;
       hourLaterInputs.clock = new Date(now + (60 * 60 * 1000)).toISOString();
-      var hourLater = require('../lib/iob')(hourLaterInputs)[0];
+      var hourLater = generate(hourLaterInputs)[0];
       hourLater.iob.should.be.lessThan(1);
       hourLater.bolussnooze.should.be.lessThan(.5);
       hourLater.iob.should.be.greaterThan(0);
+      hourLater.activity.should.be.greaterThan(0);
 
       var afterDIAInputs = inputs;
       afterDIAInputs.clock = new Date(now + (3 * 60 * 60 * 1000)).toISOString();
-      var afterDIA = require('../lib/iob')(afterDIAInputs)[0];
+      var afterDIA = generate(afterDIAInputs)[0];
 
       afterDIA.iob.should.equal(0);
       afterDIA.bolussnooze.should.equal(0);
-    });
+      afterDIA.activity.should.equal(0);
+    }
   });
 
   it('should snooze fast if bolussnooze_dia_divisor is high', function() {
@@ -76,17 +83,23 @@ describe('IOB', function ( ) {
           type: 'Bolus'
           , amount: 1
           , start_at: timestamp
+          , end_at: timestamp
+          , unit: "U"
         }]
         , profile: profile
-        , prepared: true
       };
 
-    [inputsDefault, inputsPrepared].forEach(function(inputs) {    
+    runTest(require('../lib/iob'), inputsDefault);
+    runTest(require('../lib/iob-prepared'), inputsPrepared);
+      
+    function runTest(generate, inputs) {
+      var rightAfterBolus = generate(inputs)[0];
+      rightAfterBolus.bolussnooze.should.equal(1);
       var snoozeInputs = inputs;
       snoozeInputs.clock = new Date(now + (20 * 60 * 1000)).toISOString();
-      var snooze = require('../lib/iob')(snoozeInputs)[0];
+      var snooze = generate(snoozeInputs)[0];
       snooze.bolussnooze.should.equal(0);
-    });
+    }
   });
 
   it('should calculate IOB with Temp Basals', function() {
@@ -112,20 +125,23 @@ describe('IOB', function ( ) {
           , start_at: timestampEarly
           , end_at: timestamp
           , amount: 1
-          , description: 'TempBasal: 1.0U/hour over 30min'
+          , unit: "U/hour"
         }]
         , profile: profile
-        , prepared: true
       };
 
-    [inputsDefault, inputsPrepared].forEach(function(inputs) {    
+    runTest(require('../lib/iob'), inputsDefault);
+    runTest(require('../lib/iob-prepared'), inputsPrepared);
+      
+    function runTest(generate, inputs) {
       var hourLaterInputs = inputs;
       hourLaterInputs.clock = new Date(now + (60 * 60 * 1000)).toISOString();
-      var hourLater = require('../lib/iob')(hourLaterInputs)[0];
+      var hourLater = generate(hourLaterInputs)[0];
       
       hourLater.iob.should.be.lessThan(1);
       hourLater.iob.should.be.greaterThan(0);
-    });   
+      hourLater.activity.should.be.greaterThan(0);
+    }
   });
 
   it('should calculate IOB with Temp Basal events that overlap', function() {
@@ -173,20 +189,23 @@ describe('IOB', function ( ) {
           , start_at: timestampEarly
           , end_at: timestamp
           , amount: -1
-          , description: "TempBasal: -1.0U/hour over 30min"
+          , unit: "U/hour"
         }]
         , profile: profile
-        , prepared: true
       };
 
-    [inputsDefault, inputsPrepared].forEach(function(inputs) {    
+    runTest(require('../lib/iob'), inputsDefault);
+    runTest(require('../lib/iob-prepared'), inputsPrepared);
+      
+    function runTest(generate, inputs) {
       var hourLaterInputs = inputs;
       hourLaterInputs.clock = new Date(now + (60 * 60 * 1000)).toISOString();
-      var hourLater = require('../lib/iob')(hourLaterInputs)[0];
+      var hourLater = generate(hourLaterInputs)[0];
       
       hourLater.iob.should.be.lessThan(0);
       hourLater.iob.should.be.greaterThan(-1);
-    });
+      hourLater.activity.should.be.lessThan(0);
+    }
   });
 
   it('should show 0 IOB with Temp Basals if duration is not found', function() {
@@ -252,33 +271,102 @@ describe('IOB', function ( ) {
           type: 'Bolus'
           , amount: 1
           , start_at: timestamp
+          , end_at: timestamp
+          , unit: "U"
         }]
         , profile: profile
-        , prepared: true
       };
 
-    [inputsDefault, inputsPrepared].forEach(function(inputs) {    
-      var rightAfterBolus = require('../lib/iob')(inputs)[0];
+    runTest(require('../lib/iob'), inputsDefault);
+    runTest(require('../lib/iob-prepared'), inputsPrepared);
+
+    function runTest(generate, inputs) {
+      var rightAfterBolus = generate(inputs)[0];
       //console.log(rightAfterBolus);
       rightAfterBolus.iob.should.equal(1);
       rightAfterBolus.bolussnooze.should.equal(1);
+      rightAfterBolus.activity.should.equal(0);
 
       var hourLaterInputs = inputs;
       hourLaterInputs.clock = new Date(now + (60 * 60 * 1000)).toISOString();
-      var hourLater = require('../lib/iob')(hourLaterInputs)[0];
+      var hourLater = generate(hourLaterInputs)[0];
       hourLater.iob.should.be.lessThan(1);
       hourLater.bolussnooze.should.be.lessThan(.5);
       hourLater.iob.should.be.greaterThan(0);
+      hourLater.activity.should.be.greaterThan(0);
 
       var after3hInputs = inputs;
       after3hInputs.clock = new Date(now + (3 * 60 * 60 * 1000)).toISOString();
-      var after3h = require('../lib/iob')(after3hInputs)[0];
+      var after3h = generate(after3hInputs)[0];
       after3h.iob.should.be.greaterThan(0);
+      after3h.activity.should.be.greaterThan(0);
 
       var after4hInputs = inputs;
       after4hInputs.clock = new Date(now + (4 * 60 * 60 * 1000)).toISOString();
-      var after4h = require('../lib/iob')(after4hInputs)[0];
+      var after4h = generate(after4hInputs)[0];
       after4h.iob.should.equal(0);
-    });
+      after4h.activity.should.equal(0);
+    }
+  });
+
+  it('should calculate IOB with Square Boluses', function() {
+    
+    // we should make sure cancelled square boluses are handled correctly
+    // we should make sure square boluses in progress are handled correctly
+    // we should make sure suspend/resumes are handled correctly
+
+    var now = Date.now()
+      , timestamp = new Date(now).toISOString()
+      , timestampEarly = new Date(now - (30 * 60 * 1000)).toISOString()
+      , profile = { dia: 3, bolussnooze_dia_divisor: 2}
+      , inputsDefault = {
+        clock: timestamp
+        , history: [{
+          _type: 'Bolus'
+          , type: 'square'
+          , duration: 30
+          , amount: 1
+          , timestamp: timestamp
+        }]
+        , profile: profile
+      }
+      , inputsPrepared = {
+        clock: timestamp
+        , history: [{
+          amount: 2
+          , start_at: timestampEarly
+          , description: "Square bolus: 1.0U over 30min"
+          , type: 'Bolus'
+          , unit: "U/hour"
+          , end_at: timestamp
+        }]
+        , profile: profile
+      };
+
+    runTest(require('../lib/iob'), inputsDefault);
+    runTest(require('../lib/iob-prepared'), inputsPrepared);
+      
+    function runTest(generate, inputs) {
+      var rightAfterBolus = generate(inputs)[0];
+      rightAfterBolus.iob.should.be.lessThan(1);
+      rightAfterBolus.bolussnooze.should.be.lessThan(1);
+      rightAfterBolus.activity.should.be.greaterThan(0);
+
+      var hourLaterInputs = inputs;
+      hourLaterInputs.clock = new Date(now + (60 * 60 * 1000)).toISOString();
+      var hourLater = generate(hourLaterInputs)[0];
+      hourLater.iob.should.be.lessThan(rightAfterBolus.iob);
+      hourLater.bolussnooze.should.be.lessThan(.5 * rightAfterBolus.bolussnooze);
+      hourLater.iob.should.be.greaterThan(0);
+      hourLater.activity.should.be.greaterThan(0);
+
+      var afterDIAInputs = inputs;
+      afterDIAInputs.clock = new Date(now + (3 * 60 * 60 * 1000)).toISOString();
+      var afterDIA = generate(afterDIAInputs)[0];
+
+      afterDIA.iob.should.equal(0);
+      afterDIA.bolussnooze.should.equal(0);
+      afterDIA.activity.should.equal(0);     
+    }
   });
 });
