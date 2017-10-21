e5bcb25c301d0f60e6d24dbe5add70fc76cb8403
Paul Dickens <dickens.paul@gmail.com>
2017-10-22 10:27:36 +1100

    add xdrip logging

--- bin/monitor-xdrip.sh
+++ bin/monitor-xdrip.sh
@@ -5,6 +5,7 @@ cp -rf xdrip/glucose.json xdrip/last-glucose.json
 curl --compressed -s http://localhost:5000/api/v1/entries?count=288 | json -e "this.glucose = this.sgv" > xdrip/glucose.json
 if ! cmp --silent xdrip/glucose.json xdrip/last-glucose.json; then
     if cat xdrip/glucose.json | json -c "minAgo=(new Date()-new Date(this.dateString))/60/1000; return minAgo < 10 && minAgo > -5 && this.glucose > 38" | grep -q glucose; then
+        echo New glucose data downloaded from xDrip.
         cp -up xdrip/glucose.json monitor/glucose.json
     else
         echo No recent glucose data downloaded from xDrip.
